79b967da9dced6f57f138392e4c5aa9755ce4750
Yoon Oh <yoonoh.cyoh@gmail.com>
2026-02-02 13:21:13 -0500

    some changes

--- week2/testBenchmark.py
+++ week2/testBenchmark.py
@@ -21,6 +21,9 @@ sizes = [500, 1000, 2000, 5000, 10000, 20000, 30000, 50000, 75000, 100000] #10 d
 times_FlatL2 = []
 times_FlatIP = []
 times_HNSWFlat = []
+times_SQ_L2 = []
+times_SQ_IP = []
+
 
 for n in sizes:
     data = np.random.random((n, d)).astype("float32")
@@ -30,7 +33,7 @@ for n in sizes:
     print(n, t)
 
 for n in sizes:
-    data = np.random.random((n, d)).astype("float32")
+    data = np.random.random((n, d)).astype("float32") #normalization needed?
     index = faiss.IndexFlatIP(d)  
     t = benchmark(index, data, queries, k=5)
     times_FlatIP.append(t)
@@ -43,10 +46,34 @@ for n in sizes:
     times_HNSWFlat.append(t)
     print(n, t)
 
-#plotting
+
+for n in sizes:
+    data = np.random.random((n, d)).astype("float32")
+    index = faiss.IndexScalarQuantizer(d, faiss.ScalarQuantizer.QT_8bit, faiss.METRIC_L2)
+    index.train(data)
+    t = benchmark(index, data, queries, k=5)
+    times_SQ_L2.append(t)
+    print(n, t)
+
+for n in sizes:
+    data = np.random.random((n, d)).astype("float32")
+    index = faiss.IndexScalarQuantizer(d, faiss.ScalarQuantizer.QT_8bit, faiss.METRIC_INNER_PRODUCT)
+    index.train(data)
+    t = benchmark(index, data, queries, k=5)
+    times_SQ_IP.append(t)
+    print(n, t)
+
+
+
+
+#plotting graph
 plt.plot(sizes, times_FlatL2, label="FlatL2")
 plt.plot(sizes, times_FlatIP, label="FlatIP")
 plt.plot(sizes, times_HNSWFlat, label="HNSW")
+plt.plot(sizes, times_SQ_L2, label="SQ_L2")
+plt.plot(sizes, times_SQ_IP, label="SQ_IP")
+
+
 
 plt.xlabel("Number of Vectors")
 plt.ylabel("Total time")
